b3d66db84bcaae5922c1bcf87fa5b65b56bf91e1
Makus_Frig <72632895+MakusFrig@users.noreply.github.com>
2021-06-24 22:06:10 -0700

    Update main.py
    these are some updates from when I actually started to test my script, I tested it against another picture of iron as well as a 256x256 square of red. It returned 75% matches for the actual iron picture, and it return 1% and less matches for the red square

--- main.py
+++ main.py
@@ -10,15 +10,46 @@ def get_files():
             temp.append(i.split(".")[0])
     return temp
     
+def test(pic, a_t): #a type picture and a type average_type
+    temp_margin = error_margin(a_t.average)
+    if pic.average[0] > a_t.average[0]:
+        r_percent = (temp_margin.r_error[0]-(pic.average[0]-a_t.average[0]))/temp_margin.r_error[0]
+    else:
+        r_percent = pic.average[0]/temp_margin.r_error[1]
+        
+        
+        
+    if pic.average[1] > a_t.average[1]:
+        g_percent = (temp_margin.g_error[0]-(pic.average[1]-a_t.average[1]))/temp_margin.g_error[0]
+    else:
+        g_percent = pic.average[1]/temp_margin.g_error[1]
+        
+        
+        
+    if pic.average[2] > a_t.average[2]:
+        b_percent = (temp_margin.b_error[0]-(pic.average[2]-a_t.average[2]))/temp_margin.b_error[0]
+    else:
+        b_percent = pic.average[2]/temp_margin.b_error[1]
+    print(r_percent, g_percent, b_percent)
 
 def main():
     temp = get_files()
     starting = dict()
     for i in range(5):
         starting[str(i+1)] = picture(temp[i])
-        starting[str(i+1)].give_info()
     for i in starting:
         starting[i].give_info()
+        
+    iron = average_type(starting, "iron")
+    test_image = picture("test_image")
+    iron.give_info()
+    test_image.give_info()
+    test(test_image, iron)
+    
+    
+    test_image2 = picture("test_image2")
+    test_image2.give_info()
+    test(test_image2, iron)
 
 
 
