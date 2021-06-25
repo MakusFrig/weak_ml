a5de05cae603fe1f747cbaeba0fe75d9e9dbded7
Makus_Frig <72632895+MakusFrig@users.noreply.github.com>
2021-06-25 13:13:44 -0700

    Update main.py
    I added three "if" statements so that there would be warnings if the tested material falls outside of the maximum or minimum range of RGB values of a certain type

--- main.py
+++ main.py
@@ -12,24 +12,45 @@ def get_files():
     
 def test(pic, a_t): #a type picture and a type average_type
     temp_margin = error_margin(a_t.average)
+    '''
+    the following if else statements calculate the percentage of how far off the rgb values are
+    from the average rgb values, they then print this to the screen
+    
+    '''
+    
     if pic.average[0] > a_t.average[0]:
-        r_percent = (temp_margin.r_error[0]-(pic.average[0]-a_t.average[0]))/temp_margin.r_error[0]
+        r_percent = (temp_margin.r_error[0]-(pic.average[0]-a_t.average[0]))/temp_margin.r_error[0]*100
     else:
-        r_percent = pic.average[0]/temp_margin.r_error[1]
+        r_percent = pic.average[0]/temp_margin.r_error[1]*100
         
         
         
     if pic.average[1] > a_t.average[1]:
-        g_percent = (temp_margin.g_error[0]-(pic.average[1]-a_t.average[1]))/temp_margin.g_error[0]
+        g_percent = (temp_margin.g_error[0]-(pic.average[1]-a_t.average[1]))/temp_margin.g_error[0]*100
     else:
-        g_percent = pic.average[1]/temp_margin.g_error[1]
+        g_percent = pic.average[1]/temp_margin.g_error[1]*100
         
         
         
     if pic.average[2] > a_t.average[2]:
-        b_percent = (temp_margin.b_error[0]-(pic.average[2]-a_t.average[2]))/temp_margin.b_error[0]
+        b_percent = (temp_margin.b_error[0]-(pic.average[2]-a_t.average[2]))/temp_margin.b_error[0]*100
     else:
-        b_percent = pic.average[2]/temp_margin.b_error[1]
+        b_percent = pic.average[2]/temp_margin.b_error[1]*100
+        
+    '''
+    The next three if statements are for if one of the rgb values is outside of the minimum and/or maximum
+    values which are a part of the average_type
+    '''
+        
+    if pic.average[0] > a_t.maximum[0] or pic.average[0] < a_t.minimum[0]:
+        print("Warning, \"r\" value maximum/minium dependencies not met")
+    
+    if pic.average[1] > a_t.maximum[1] or pic.average[1] < a_t.minimum[1]:
+        print("Warning, \"g\" value maximum/minium dependencies not met")
+        
+    if pic.average[2] > a_t.maximum[2] or pic.average[2] < a_t.minimum[2]:
+        print("Warning, \"b\" value maximum/minium dependencies not met")
+    
     print(r_percent, g_percent, b_percent)
 
 def main():
